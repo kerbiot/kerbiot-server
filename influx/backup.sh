3f09181209bbf8c84458fccc6105b440ad5b0a9a
NicolasBissig <nicbissig@gmail.com>
2022-09-22 19:48:23 +0200

    remove url scheme from query

--- influx/backup.sh
+++ influx/backup.sh
@@ -8,7 +8,7 @@ BACKUP_NAME=$(date +"%Y-%m-%d-%H-%M-%S")
 QUERY="from(bucket: \"$INFLUX_BUCKET\") |> range(start: 0, stop: now())"
 
 echo "Backing up" $INFLUX_HOST
-eval ./influx query --host 'https://'$INFLUX_HOST -o $INFLUX_ORG -t $INFLUX_TOKEN \'$QUERY\' --raw > $BACKUP_NAME.csv
+eval ./influx query --host $INFLUX_HOST -o $INFLUX_ORG -t $INFLUX_TOKEN \'$QUERY\' --raw > $BACKUP_NAME.csv
 
 zip $BACKUP_NAME.zip $BACKUP_NAME.csv 
 rm $BACKUP_NAME.csv 
